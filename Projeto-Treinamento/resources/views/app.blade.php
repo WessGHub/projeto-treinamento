4e9787bb2e30f1f9cabfcb502e7db2776b167eef
Wesley <wesleullmann.nunesy@email.com>
2025-08-19 13:45:34 -0300

    href mudada

--- Projeto-Treinamento/resources/views/app.blade.php
+++ Projeto-Treinamento/resources/views/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/src/style.css" rel="stylesheet">
+    <link href="/public/css/styles.css" rel="stylesheet">
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
